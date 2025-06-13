524e732f4cab04172ac0ec2d88bf7ffe8b9ec5bd
Charles <c4a@monocles.eu>
2025-06-13 13:36:14 -0400

    fix gitignore

---
+++ mcwrap.py
@@ -0,0 +1,25 @@
+import download_assets
+import download_client_jar
+import download_libs
+import run_client
+import requests
+import sys
+import json
+import os
+import glob
+
+os.makedirs("mc" + sys.argv[2].split("/")[-1].split(".json")[0], exist_ok=True)
+os.chdir("mc" + sys.argv[2].split("/")[-1].split(".json")[0])
+
+data = json.loads(requests.get(sys.argv[2]).text)
+
+download_client_jar.download_client_jar(data["downloads"]["client"]["url"])
+
+download_libs.download_libs(data["libraries"])
+
+assets = json.loads(requests.get(data["assetIndex"]["url"]).content)
+download_assets.download_assets(assets["objects"])
+
+run_client.run_client(sys.argv[1], data["id"])
+
+os.chdir("..")
